81b6b05bd4b0cf71393aa3c4f95601a2fcdd37d4
Synapse Pvt Ltd <Kudakwashesevenzo1@gmail.com>
2026-03-29 14:51:26 +0200

    update project files

---
+++ .next/static/chunks/app/page-23f9c1759a70ae8a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{5574:(e,s,n)=>{Promise.resolve().then(n.bind(n,6613)),Promise.resolve().then(n.t.bind(n,1356,23))}},e=>{e.O(0,[204,441,255,358],()=>e(e.s=5574)),_N_E=e.O()}]);
